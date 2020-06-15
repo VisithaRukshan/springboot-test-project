425e2ef565e147ff3e5f4cbecd700ea4fbe3c949
Visitha Perera <Visitha Perera@DESKTOP-OOG1RE1>
2020-06-15 19:56:50 +0530

    create the project and auto wire and dependency injection part 1

---
+++ test1/src/main/java/com/example/demo/Test1Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Test1Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Test1Application.class, args);
+		
+		Test t1 = context.getBean(Test.class);
+		
+		t1.show();
+	}
+
+}
